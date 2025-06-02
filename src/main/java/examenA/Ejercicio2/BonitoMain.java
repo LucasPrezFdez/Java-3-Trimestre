521bda7fc4ea4d165a161c5f933c9227f149c661
Lukas <lucaslinoperez@gmail.com>
2025-06-02 09:06:43 +0200

    fix

--- src/main/java/examenA/Ejercicio2/BonitoMain.java
+++ src/main/java/examenA/Ejercicio2/BonitoMain.java
@@ -12,17 +12,19 @@ public class BonitoMain {
 
         Random random = new Random();
 
-        Producto producto1 = new Producto("Carpeta",1, "Carpeta anillada metalica", 1 + random.nextFloat(200-1));
-        Producto producto2 = new Producto("Lapiz",2, "Lapiz de grafito", 1 + random.nextFloat(200-1));
-        Producto producto3 = new Producto("Boligrafo",3, "Boligrafo azul", 1 + random.nextFloat(200-1));
-        Producto producto4 = new Producto("Goma",4, "Goma de borrar", 1 + random.nextFloat(200-1));
+        Producto producto1 = new Producto("Carpeta", 1, "Carpeta anillada metalica", 1 + random.nextFloat(200 - 1));
+        Producto producto2 = new Producto("Lapiz", 2, "Lapiz de grafito", 1 + random.nextFloat(200 - 1));
+        Producto producto3 = new Producto("Boligrafo", 3, "Boligrafo azul", 1 + random.nextFloat(200 - 1));
+        Producto producto4 = new Producto("Goma", 4, "Goma de borrar", 1 + random.nextFloat(200 - 1));
+        Producto producto5 = new Producto("Mochila", 5, "Mochila molona", 1 + random.nextFloat(200 - 1));
 
         Orden factura1 = new Orden(123);
 
-        factura1.añadirLinea(1, random.nextInt(6) + 1, producto1);
-        factura1.añadirLinea(2, random.nextInt(6) + 1, producto2);
-        factura1.añadirLinea(3, random.nextInt(6) + 1, producto3);
-        factura1.añadirLinea(4, random.nextInt(6) + 1, producto4);
+        factura1.añadirLinea(1, random.nextInt(10) + 1, producto1);
+        factura1.añadirLinea(2, random.nextInt(10) + 1, producto2);
+        factura1.añadirLinea(3, random.nextInt(10) + 1, producto3);
+        factura1.añadirLinea(4, random.nextInt(10) + 1, producto4);
+        factura1.añadirLinea(5, random.nextInt(10) + 1, producto5);
 
         factura1.calcularTotalFacturaConIVA();
 
@@ -31,13 +33,11 @@ public class BonitoMain {
         try (FileWriter writer = new FileWriter("LucasPerez.txt")) {
             writer.write(factura1.toString());
             writer.write("\n--------------------------------------------------\n");
-            writer.write("Total factura con IVA (17%): €" + factura1.calcularTotalFacturaConIVA());
+            writer.write("Total factura con IVA (17%): " + String.format("%.2f", factura1.calcularTotalFacturaConIVA()) + " €");
             System.out.println("El ticket se ha guardado en el fichero LucasPerez.txt");
         } catch (IOException e) {
             System.err.println("Error al guardar el ticket: " + e.getMessage());
         }
-
-
 
 
     }
--- src/main/java/examenA/Ejercicio2/Orden.java
+++ src/main/java/examenA/Ejercicio2/Orden.java
@@ -46,21 +46,21 @@ public class Orden {
 
     @Override
     public String toString() {
-        return "Orden [id = " +identificador+"]\n" +
+        return "Orden [id = " + identificador + "]\n" +
                 "Items de la factura\n" +
                 itemsFactura + "\n" +
                 "Total orden = € " + calcularTotalFactura();
     }
 
     public void imprimir() {
-        System.out.println("Factura [id = "+identificador+"]\n");
-        for (LineaPedido l : itemsFactura){
+        System.out.println("Factura [id = " + identificador + "]\n");
+        for (LineaPedido l : itemsFactura) {
             LineaPedido linea = (LineaPedido) l;
-            System.out.println(identificador+", cantidad= "+l.getCantidad()+", nombre= "
-                    +l.getProducto().getNombre()+", precio= €"+l.getProducto().getPrecio()+"subtotal= €"+linea.calcularSubtotalLineaPedido()+"  ]");
+            System.out.println(identificador + ", cantidad= " + l.getCantidad() + ", nombre= "
+                    + l.getProducto().getNombre() + ", precio= €" + l.getProducto().getPrecio() + "subtotal= €" + linea.calcularSubtotalLineaPedido() + "  ]");
         }
         System.out.println("--------------------------------------------------");
-        System.out.println("Total factura = € "+calcularTotalFactura());
+        System.out.println("Total factura = € " + calcularTotalFactura());
     }
 
     public double calcularTotalFacturaConIVA() {
@@ -68,10 +68,11 @@ public class Orden {
         for (LineaPedido linea : itemsFactura) {
             total += linea.calcularSubtotalLineaPedido();
         }
-        double iva = total * 0.17; // Cálculo del IVA (17%)
+        double iva = total * 0.15; // Cálculo del IVA (17%)
         return total + iva;
 
     }
+
     public void imprimirConIVA() {
         System.out.println("Factura [id = " + identificador + "]\n");
         for (LineaPedido linea : itemsFactura) {
@@ -82,6 +83,6 @@ public class Orden {
                     ", Subtotal: €" + linea.calcularSubtotalLineaPedido());
         }
         System.out.println("--------------------------------------------------");
-        System.out.println("Total factura con IVA (17%): €" + calcularTotalFacturaConIVA());
+        System.out.println("Total factura con IVA (17%): " + String.format("%.2f", calcularTotalFacturaConIVA()) + " €");
     }
 }
